95133a763c6ed535a6c3459d522372b58d5a11c3
Neidson de Oliveira <130571881+neids0n@users.noreply.github.com>
2023-04-24 16:26:19 -0300

    Validando IP do servidor zabbix

--- zabbix-agent.sh
+++ zabbix-agent.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+# Solicita e valida IP do servidor zabbix
+valida_ip(){
+    echo "Informe o IP do servidor zabbix: "
+    read ip_servidor
+
+    echo "Tem certeza que o IP $ip_servidor está correto [s/n]?"
+    read resposta
+    if [ $resposta = n ]
+    then
+        valida_ip
+    else
+        if [ $resposta != s ]
+        then
+            echo "Resposta incorreta"
+            valida_ip
+        fi
+    fi
+}
+valida_ip
 # Instalação do agent zabbix
 apt-get update && apt-get install zabbix-agent -y && \
 # Backup do arquivo zabbix_agentd.conf original
 cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.old && \
 # Alteração do IP do servidor
-sed -i 's/Server=127.0.0.1/Server=IP_DO_SERVIDOR/g' /etc/zabbix/zabbix_agentd.conf && \
-sed -i 's/ServerActive=127.0.0.1/ServerActive=IP_DO_SERVIDOR/g' /etc/zabbix/zabbix_agentd.conf && \
+sed -i 's/Server=127.0.0.1/Server='$ip_servidor'/g' /etc/zabbix/zabbix_agentd.conf && \
+sed -i 's/ServerActive=127.0.0.1/ServerActive='$ip_servidor'/g' /etc/zabbix/zabbix_agentd.conf && \
 # Reiniciar e habilitar o serviço do zabbix na inicialização do servidor
 /etc/init.d/zabbix-agent restart && systemctl enable zabbix-agent.service && \
 # Criação de regra do Iptables para habilitar porta 10050
